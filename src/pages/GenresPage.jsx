ec8d0e864d351e0c8702538a58275229affa5868
Kyd <kyd.kitchaiya@medieinstitutet.se>
2021-09-15 11:16:29 +0200

    edit genre page children

--- src/pages/GenresPage.jsx
+++ src/pages/GenresPage.jsx
@@ -1,28 +1,72 @@
 import { useQuery } from "react-query";
-import { getGenres } from "../services/fetchData";
 import { useParams } from "react-router";
+import { useState } from "react";
+//API
+import { getGenre } from "../services/fetchData";
+import { getGenres } from "../services/fetchData";
 //components
 import GenreButtons from "../components/GenreButtons";
-import GenreDetails2 from "../components/GenreDetails2";
-import WelcomeGenreDetails from "../components/WelcomeGenreDetails";
+import Pagination from "../components/Pagination";
+import MovieCard from "../components/MovieCard";
 //Styling
 import gridStyle from "../css/Grid.module.css";
 
 const GenrePage = () => {
-  const { id } = useParams();
-  const { data, isLoading, error, isError } = useQuery("getGenres", () => {
+  const { id, genretype } = useParams();
+  const [page, setPage] = useState(1);
+
+  const {
+    data: buttonData,
+    isLoading: buttonIsLoading,
+    error: buttonError,
+    isError: buttonIsError,
+  } = useQuery("getGenres", () => {
     return getGenres();
   });
+
+  const { data, isLoading, error, isError, isPreviousData } = useQuery(
+    ["getGenre", id, page],
+    () => {
+      return getGenre(id, page);
+    }
+  );
+
+  // useEffect(() => {
+  //   data && setPage(data.page);
+  // }, [data]);
 
   return (
     <div className={gridStyle.supercontainer}>
       <h1>Genres</h1>
+
       <div>
-        {isLoading && <p>Loading....</p>}
-        {isError && <p>There has been an error: {error}</p>}
-        {data?.genres && <GenreButtons genreButtons={data.genres} />}
+        {buttonIsLoading && <p>Loading....</p>}
+        {buttonIsError && <p>There has been an error: {buttonError}</p>}
+        {buttonData?.genres && (
+          <GenreButtons genreButtons={buttonData.genres} />
+        )}
       </div>
-      {id ? <GenreDetails2 /> : <WelcomeGenreDetails />}
+
+      {data?.results && (
+        <div>
+          <h1>Movies from {genretype}</h1>
+          <div className={gridStyle.container}>
+            {isLoading && <p>Loading....</p>}
+            {isError && <p>There has been an error: {error}</p>}
+            {data?.results && (
+              <>
+                <MovieCard movies={data.results} />
+                <Pagination
+                  page={page}
+                  setPage={setPage}
+                  isPreviousData={isPreviousData}
+                  hasMore={data.results[0]}
+                />
+              </>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
